f60f31c2ef2f29bbb1408ad6e0daeeabbddee43c
gilgameshskytrooper <leeas@stolaf.edu>
2018-05-02 12:04:25 -0500

    updates

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gilgameshskytrooper/pausepizza/src/kitchen_web_server/utils"
 )
 
 func main() {
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		http.ServeFile(w, r, utils.Pwd()+"index.html")
 	})
 
 	http.HandleFunc("/mess/ws", func(w http.ResponseWriter, r *http.Request) {
